30ec297e6fa43684467bf8aeeca2717e44d6172e
Mike Butler <mike.e.butler@gmail.com>
2016-10-10 04:54:07 -0500

    Add solution for Life, the Universe, and Everything
    	* Added solution for Life, the Universe, and Everything
    
    Supports [LifeUniverseEverything]

---
+++ spoj/LifeUniversEverything/LifeUniversEverything/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LifeUniversEverything
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			while (true)
+			{
+				var inputstring = Console.ReadLine().Trim();
+				var inputvalue = Int32.Parse(inputstring);
+
+				if (inputvalue == 42)
+				{
+					break;
+				}
+				Console.WriteLine(inputvalue);
+			}
+		}
+	}
+}
