07aa71b4f743e8ece45241bb10db6840ede66def
Mike Butler <mike.e.butler@gmail.com>
2016-10-10 04:32:28 -0500

    Adding solutions for DivSum and STRHH
    	* Add solution for spoj/DivSum
    	* Add solution for spoj/STRHH
    
    Supports [DeliberatePractice]

---
+++ spoj/DivSum/DivSum/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DivSum
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			List<uint> resultList = new List<uint>();
+			try
+			{
+				string inputline = Console.ReadLine().Trim();
+				uint inputvalue = UInt32.Parse(inputline);
+				uint inputsize = inputvalue;
+
+				for (var index1 = 0; index1 < inputsize; index1 += 1)
+				{
+					inputline = Console.ReadLine().Trim();
+					inputvalue = UInt32.Parse(inputline);
+
+					uint theSum = 1;
+					uint temp = 0;
+
+					uint limit = (uint)Math.Sqrt(inputvalue);
+					if (inputvalue == 1)
+					{
+						theSum = 0;
+					}
+
+					//Console.WriteLine("Processing [" + inputvalue + "] to [" + limit + "]");
+					for (uint i = 2; i <= limit; i += 1)
+					{
+						temp = 0;
+						if (inputvalue % i == 0)
+						{
+							//Console.WriteLine();
+							//Console.Write("Adding divisor [" + i + "]");
+							theSum += i;
+							temp = inputvalue / i;
+							if (i != temp)
+							{
+								//Console.Write(", Adding divisor [" + temp + "]");
+								theSum += temp;
+							}
+						}
+					}
+					//Console.WriteLine(", Sum [" + theSum + "]");
+					resultList.Add(theSum);
+				}
+				foreach (var result in resultList)
+				{
+					Console.WriteLine(result);
+				}
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.WriteLine(ex.StackTrace);
+			}
+
+		}
+	}
+}
---
+++ spoj/STRHH/STRHH/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace STRHH
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Get number of test cases
+			var inputstring = Console.ReadLine();
+			int testcases = int.Parse(inputstring.Trim());
+
+			for (int caseindex = 1; caseindex <= testcases; caseindex += 1)
+			{
+				bool printit = true;
+				string inputcase = Console.ReadLine().Trim();
+				var inputArray = inputcase.ToCharArray();
+
+				int loopsize = (int)(inputcase.Length / 2);
+
+				for (int idx0 = 0; idx0 < loopsize; idx0 += 1)
+				{
+					if (printit)
+					{
+						Console.Write(inputArray[idx0]);
+					}
+					printit = !printit;
+				}
+				Console.WriteLine("");
+			}
+		}
+	}
+}
